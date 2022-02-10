42ee9a8392c7a5d4af07070797d262361f869f50
Andrew Chong <i@andrewhchong.com>
2022-02-10 14:30:57 -0800

    Update app to run on heroku

--- votr-flask/run.py
+++ votr-flask/run.py
@@ -4,3 +4,8 @@ from votr_flask.app import create_app
 from flask import Flask
 
 app = create_app()
+
+
+if __name__ == "__main__":
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True)
