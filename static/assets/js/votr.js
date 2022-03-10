108955973413c23a1e196085284d2198cf10f0f9
Andrew Chong <i@andrewhchong.com>
2022-03-09 20:00:20 -0800

    Add find_user_by_id behavior

--- static/assets/js/votr.js
+++ static/assets/js/votr.js
@@ -2,11 +2,67 @@ $(document).ready(function () {
   $('[data-toggle="tooltip"]').tooltip();
 });
 
-$(document).ready(function () {
-  console.log("workin");
-  $("#find-user-by-id").submit(function (event) {
-    console.log("Handler for search user by id .submit() called.");
-    alert("Handler for search user by id .submit() called.");
+// TODO: Maybe replace jQuery document.ready with native JS
+$(document).ready(function handleFindUserById() {
+  console.log("working");
+  $("#users-find-by-id-btn").click(async function (event) {
     event.preventDefault();
+
+    let findUserByIdForm = document.getElementById("find-user-by-id-form");
+
+    let response = await fetch("find_user_by_id", {
+      method: "POST",
+      body: new FormData(findUserByIdForm),
+    });
+
+    let user_data = await response.json();
+
+    document.getElementById(
+      "users-find-by-id-username"
+    ).value = `${user_data.username}`;
+
+    document.getElementById(
+      "users-find-by-id-email"
+    ).value = `${user_data.email_address}`;
+
+    if (user_data.site_permissions.includes("guest")) {
+      document.getElementById(
+        "users-find-by-id-site-permissions-guest"
+      ).checked = true;
+    } else {
+      document.getElementById(
+        "users-find-by-id-site-permissions-guest"
+      ).checked = false;
+    }
+
+    if (user_data.site_permissions.includes("user")) {
+      document.getElementById(
+        "users-find-by-id-site-permissions-user"
+      ).checked = true;
+    } else {
+      document.getElementById(
+        "users-find-by-id-site-permissions-user"
+      ).checked = false;
+    }
+
+    if (user_data.site_permissions.includes("admin")) {
+      document.getElementById(
+        "users-find-by-id-site-permissions-admin"
+      ).checked = true;
+    } else {
+      document.getElementById(
+        "users-find-by-id-site-permissions-admin"
+      ).checked = false;
+    }
+
+    if (user_data.site_permissions.includes("superadmin")) {
+      document.getElementById(
+        "users-find-by-id-site-permissions-superadmin"
+      ).checked = true;
+    } else {
+      document.getElementById(
+        "users-find-by-id-site-permissions-superadmin"
+      ).checked = false;
+    }
   });
 });
